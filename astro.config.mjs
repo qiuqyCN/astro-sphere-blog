d6728d041b555ffac00f653218f2583c6d88b92a
Qiuyun <97208294@qq.com>
2024-10-27 10:16:57 +0800

    feat: 新增vercel 适配器

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,12 @@ import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-sphere-demo.vercel.app",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
+  output: "server",
+  adapter: vercel(),
 })
